4095f09bb24538441ded6242dd95cc929a05d058
Fernando Aguilar Marin <fernandoaguilarmarin13@gmail.com>
2026-03-06 17:16:49 -0600

    Actualizar scraper metro

--- scrapingMetro.js
+++ scrapingMetro.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
 const admin = require("firebase-admin");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
@@ -9,19 +11,30 @@ admin.initializeApp({
 const db = admin.firestore();
 
 async function scrape() {
-  try {
-    console.log("Probando escritura directa...");
 
-    await db.collection("estado_servicio").doc("metro").set({
-      estado: "Prueba Automática",
-      actualizado: new Date(),
-    });
+  console.log("Iniciando scraping Metro...");
 
-    console.log("Actualizado correctamente");
-  } catch (error) {
-    console.error("Error:", error);
-    throw error; // importante para que falle el workflow si hay error
-  }
+  const estados = {
+    Linea_1: "Operando",
+    Linea_2: "Operando",
+    Linea_3: "Operando",
+    Linea_4: "Operando",
+    Linea_5: "Operando",
+    Linea_6: "Operando",
+    Linea_7: "Operando",
+    Linea_8: "Operando",
+    Linea_9: "Operando",
+    Linea_A: "Operando",
+    Linea_B: "Operando",
+    Linea_12: "Operando"
+  };
+
+  await db.collection("estado_servicio").doc("metro").update({
+    ...estados,
+    Ultima_actualizacion: new Date()
+  });
+
+  console.log("Metro actualizado");
 }
 
 scrape();
